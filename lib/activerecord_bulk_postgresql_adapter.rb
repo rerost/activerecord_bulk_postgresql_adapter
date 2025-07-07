9766959c795dfa1d91467aafced27be1c3cbdb42
Hazumi Ichijo <hahihu314+github@gmail.com>
2025-07-07 22:47:27 +0900

    Update activerecord_bulk_postgresql_adapter.rb

--- lib/activerecord_bulk_postgresql_adapter.rb
+++ lib/activerecord_bulk_postgresql_adapter.rb
@@ -175,7 +175,7 @@ module ActiveRecord
             inddef = row[3]
             comment = row[4]
             valid = row[5]
-            columns = decode_string_array(row[6]).map { |c| ActiveRecord::ConnectionAdapters::ostgreSQL::Utils.unquote_identifier(c.strip.gsub('""', '"')) }
+            columns = decode_string_array(row[6]).map { |c| ActiveRecord::ConnectionAdapters::PostgreSQL::Utils.unquote_identifier(c.strip.gsub('""', '"')) }
 
             using, expressions, include, nulls_not_distinct, where = inddef.scan(/ USING (\w+?) \((.+?)\)(?: INCLUDE \((.+?)\))?( NULLS NOT DISTINCT)?(?: WHERE (.+))?\z/m).flatten
 
